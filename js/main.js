e37194e13dbb0bc684b43d3fb12c4276aa16fc30
surajchourasiya <pallawikumaribth80@gmail.com>
2022-08-23 00:17:20 +0530

    home

---
+++ js/main.js
@@ -0,0 +1,13 @@
+//slider
+
+var swiper = new Swiper(".mySwiper", {
+  direction: "vertical",
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  autoplay: {
+    delay: 3500,
+  },
+});
