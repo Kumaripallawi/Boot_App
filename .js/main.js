ae72eab6869e6faeba5995a823fadfe062572930
surajchourasiya <pallawikumaribth80@gmail.com>
2022-09-01 18:11:24 +0530

    counter-section

--- .js/main.js
+++ .js/main.js
@@ -31,3 +31,26 @@ var swiper = new Swiper(".mySwiper", {
     delay: 3500,
   },
 });
+
+// counter-desing
+document.addEventListener("DOMContentLoaded", () => {
+  function counter(id, start, end, duration) {
+    let obj = document.getElementById(id),
+      current = start,
+      range = end - start,
+      increment = end > start ? 1 : -1,
+      step = Math.abs(Math.floor(duration / range)),
+      timer = setInterval(() => {
+        current += increment;
+        obj.textContent = current;
+        if (current == end) {
+          clearInterval(timer);
+        }
+      }, step);
+  }
+
+  counter("count1", 0, 1287, 2000);
+  counter("count2", 100, 5676, 2500);
+  counter("count3", 159, 6707, 3000);
+  counter("count4", 200, 90287, 3500);
+});
